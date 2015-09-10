060a55b0fe4489dacb7db2764b56df7b387f62ae
vladimir <vladimir@cosof.cl>
2015-09-10 18:18:25 -0300

    funcion mail

---
+++ application/models/sendmail_model.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!defined("BASEPATH"))
+    exit("No direct script access allowed");
+
+class Sendmail_Model extends CI_Model {
+
+    public function emailSend($to = null, $cc = null, $bcc = null, $subject = null, $message = null) {
+        
+        $this->load->library('email');
+
+        $this->email->from('noresponder@minsal.cl', 'No Responder - MINSAL');
+        $this->email->to($to);
+        $this->email->cc($cc);
+        if($bcc!=null)
+        {   
+            $this->email->bcc($bcc);
+        }
+        $this->email->subject($subject);
+        $this->email->message($message);
+
+        $this->email->send();
+
+        echo $this->email->print_debugger();
+    }
+
+}
