3b7ef0e1b1c53794a667ce313c6a35aebd835d03
vladimir <vladimir@cosof.cl>
2015-10-27 15:21:35 -0300

    se cargan plugin htmlcanvas

--- assets/js/exportMap.js
+++ assets/js/exportMap.js
@@ -277,11 +277,15 @@ var ExportMap = {
 
     this.renderImage = function () {
         html2canvas($('#dvMap'), {
+            useCORS: true,
             onrendered: function (canvas) {
-               document.body.appendChild(canvas);
-            },
-            allowTaint: true,
-            logging: true
+                var dataUrl= canvas.toDataURL("image/png");
+
+            // DO SOMETHING WITH THE DATAURL
+            // Eg. write it to the page
+            document.write('<img src="' + dataUrl + '"/>');
+            }
+            
         });
 
     };
