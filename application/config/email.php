00d95af335a7b0cf52b5a83e3a49d55943a360db
Carlos Ayala <carlos.ayala@cosof.cl>
2015-11-05 12:42:58 -0400

    Config

---
+++ application/config/email.php
@@ -0,0 +1,11 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config['protocol'] = 'sendmail';
+$config['mailpath'] = '/usr/sbin/sendmail';
+$config['charset'] = 'iso-8859-1';
+$config['wordwrap'] = TRUE;
+$config['smtp_host'] = "mail.minsal.cl";
+$config['smtp_user'] = "sistemas";
+$config['smtp_pass'] = "siste14S";
+$config['smtp_port'] = 25;
+$config['charset'] = "utf-8";
+$config['mailtype'] = "html";
