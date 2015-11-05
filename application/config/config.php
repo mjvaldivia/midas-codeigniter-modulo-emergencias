39715dcd4915e56960ffe48b32ed4e85e70a923c
Carlos Ayala <carlos.ayala@cosof.cl>
2015-11-05 12:23:57 -0400

    Merge

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@
 |
 */
 date_default_timezone_set("America/Argentina/Buenos_Aires");
-$config['base_url'] = 'http://localhost/emergencias';
+$config['base_url'] = 'http://sipresa.local/emergencias';
 
 /*
 |--------------------------------------------------------------------------
