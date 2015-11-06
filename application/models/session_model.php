8287d3c70ff84b5ece74cdcbdba9a05f91bffc52
vladimir <vladimir@cosof.cl>
2015-11-06 20:59:05 -0300

    avance reporte

--- application/models/session_model.php
+++ application/models/session_model.php
@@ -108,7 +108,7 @@ class Session_Model extends CI_Model
               select
               u.*,
               c.crg_c_nombre,
-              r.*,
+              r.reg_c_nombre,
               (
                 select
                   group_concat(uvo.ofi_ia_id)
@@ -202,8 +202,7 @@ class Session_Model extends CI_Model
               select
               u.*,
               c.crg_c_nombre,
-              r.*,
-              
+              r.reg_c_nombre,
               (
                 select
                   group_concat(uvo.ofi_ia_id)
@@ -272,7 +271,6 @@ class Session_Model extends CI_Model
                 $this->session->set_userdata("session_nombres", $r["usu_c_nombre"] . " " . $r["usu_c_apellido_paterno"] . " " . $r["usu_c_apellido_materno"]);
                 $this->session->set_userdata("session_usuario", $r["usu_c_login"]);
                 $this->session->set_userdata("session_region", $r["reg_c_nombre"]);
-                $this->session->set_userdata("session_region_codigo", $r["reg_ia_id"]);
                 $this->session->set_userdata("session_cargo", $r["crg_c_nombre"]);
                 $this->session->set_userdata("session_idCargo", $r["crg_ia_id"]);
                 $this->session->set_userdata("session_ambitos", $r["ambitos"]);
