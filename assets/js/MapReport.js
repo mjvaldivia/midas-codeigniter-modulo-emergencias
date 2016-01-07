901377406cf350578d1d7a5aa6ef03c4927a63c0
Carlos Ayala <carlos.ayala@cosof.cl>
2016-01-07 18:33:40 -0400

    form

--- assets/js/MapReport.js
+++ assets/js/MapReport.js
@@ -416,7 +416,7 @@ var MapReport = {
                                 $.ajax({
                                     url: siteUrl + 'visor/enviarMail/id/' + $('#eme_ia_id').val() + '/k/' + data.k,
                                     dataType: 'json',
-                                    data: {datos:formulario},
+                                    data: formulario,
                                     type: 'POST',
                                     success: function (data) {
                                         if (data.estado == 0) {
