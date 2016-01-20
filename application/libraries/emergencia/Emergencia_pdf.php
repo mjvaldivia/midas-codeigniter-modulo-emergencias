9cec7d01e1c5b7813525f90b25c93c3ef0dd74f0
Carlos Ayala <carlos.ayala@cosof.cl>
2016-01-20 18:01:16 -0400

    Test

--- application/libraries/emergencia/Emergencia_pdf.php
+++ application/libraries/emergencia/Emergencia_pdf.php
@@ -70,11 +70,12 @@ Class Emergencia_pdf{
 
         $html = $this->_ci->load->view('pages/emergencia_reporte/pdf', $data, true); 
         
-        $this->_pdf->imagen_mapa = $this->_imagen;
+       /* $this->_pdf->imagen_mapa = $this->_imagen;
         
         $this->_pdf->SetFooter($_SERVER['HTTP_HOST'] . '|{PAGENO}/{nb}|' . date('d-m-Y'));
         $this->_pdf->WriteHTML($html);
-        return $this->_pdf->Output('acta.pdf', 'S');
+        return $this->_pdf->Output('acta.pdf', 'S');*/
+        echo $html;
     }
     
     /**
