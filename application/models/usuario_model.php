2439a38ffa8c18a4389566417b344f7483e3eb2a
Ivan Almonacid <ivan@cosof.cl>
2016-01-25 18:05:46 -0300

    Correccion para registro de envio de reporte

--- application/models/usuario_model.php
+++ application/models/usuario_model.php
@@ -247,7 +247,9 @@ class Usuario_Model extends MY_Model {
         $query = "select * from usuarios where usu_c_email = ? limit 1";
         $resultado = $this->db->query($query,array($email));
         if($resultado->num_rows() > 0){
-            $usuario = $resultado[0]->result_array();
+            foreach ($result->result_array() as $row) {
+                $usuario = $row;
+            }
             return $usuario;
         }else{
             return null;
