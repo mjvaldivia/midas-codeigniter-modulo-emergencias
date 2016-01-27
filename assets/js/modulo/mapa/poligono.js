60855c665cca8bacbfb8591951b60db0c2aba877
Carlos Ayala <carlos.ayala@cosof.cl>
2016-01-27 11:45:55 -0400

    Correccion poligono

--- assets/js/modulo/mapa/poligono.js
+++ assets/js/modulo/mapa/poligono.js
@@ -55,26 +55,36 @@ var MapaPoligono = Class({
 
         var yo = this;
         
-        var poligono = new google.maps.Polygon({
-            paths: yo.coordenadas(geometry, zona),
-            identificador: id,
-            clave : "poligono_" + id,
-            capa: capa,
-            informacion: propiedades,
-            strokeColor: '#000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: color,
-            fillOpacity: 0.35
+       
+        $.each(geometry, function(i, coordenadas){
+            var puntos = [];
+            $.each(coordenadas, function(j, valores){
+                LatLng = GeoEncoder.utmToDecimalDegree(parseFloat(valores[0]), parseFloat(valores[1]), zona);
+                puntos.push(new google.maps.LatLng(parseFloat(LatLng[0]), parseFloat(LatLng[1])));
+            });
+           
+            var poligono = new google.maps.Polygon({
+                paths: puntos,
+                identificador: id,
+                clave : "poligono_" + id,
+                capa: capa,
+                informacion: propiedades,
+                strokeColor: '#000',
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: color,
+                fillOpacity: 0.35
+            });
+
+            poligono.setMap(yo.mapa);
+
+            //se agrega evento de click para ver instalaciones
+            //dentro de poligono
+            yo.addClickListener(poligono, yo.mapa);
+
+            lista_poligonos.push(poligono);
         });
-        
-        poligono.setMap(this.mapa);
-        
-        //se agrega evento de click para ver instalaciones
-        //dentro de poligono
-        this.addClickListener(poligono, this.mapa);
-        
-        lista_poligonos.push(poligono);
+
     },
     
     /**
