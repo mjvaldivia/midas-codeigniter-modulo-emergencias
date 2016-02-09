84ac3141aa551c32f4aa5d3b6aeb247cf6424e23
Ivan Almonacid <ivan@cosof.cl>
2016-02-09 16:20:37 -0300

    Se corrige visor para Reporte

--- application/controllers/emergencia.php
+++ application/controllers/emergencia.php
@@ -337,8 +337,29 @@ class Emergencia extends MY_Controller {
                 $this->emergencia_guardar->setComunas($params['comunas']);
                 $params['form_tipo_acciones'] = nl2br($params['form_tipo_acciones']);
                 $this->emergencia_guardar->guardarDatosTipoEmergencia($params);
-                
+
+                $id = $params['eme_id'];
                 //$id = $this->emergencia_guardar->getId();
+                /* verificar si existen adjuntos en temporal */
+                $directorio = 'media/tmp/';
+                $readDir = array_diff(scandir($directorio), array('..', '.'));
+                $this->load->model('archivo_model');
+                foreach($readDir as $file){
+                    if(preg_match("/^".$emergencia->eme_ia_id."-emergencia_adjunto_temp_/",$file)){
+                        if(!is_dir('media/doc/emergencia/'.$emergencia->eme_ia_id.'/adjuntos/')){
+                            mkdir('media/doc/emergencia/'.$emergencia->eme_ia_id.'/adjuntos/',0777,true);
+                        }
+                        $file_nuevo = str_replace('emergencia_adjunto_temp_','',$file);
+                        if(rename($directorio.$file, 'media/doc/emergencia/'.$emergencia->eme_ia_id.'/adjuntos/'.$file_nuevo)){
+
+                            $ruta = 'media/doc/emergencia/'.$emergencia->eme_ia_id.'/adjuntos/'.$file_nuevo;
+                            $archivo = $this->archivo_model->file_to_bd('media/doc/emergencia/'.$emergencia->eme_ia_id.'/adjuntos/', $file_nuevo, mime_content_type($ruta), $this->archivo_model->TIPO_EMERGENCIA, $emergencia->ala_ia_id, filesize($ruta));
+                            $file_nuevo = $archivo . '_'.$file_nuevo;
+                            rename($ruta,'media/doc/emergencia/'.$emergencia->eme_ia_id.'/adjuntos/'.$file_nuevo);
+                        }
+
+                    }
+                }
                 
 
                 $usuario = $this->session->userdata('session_idUsuario');
