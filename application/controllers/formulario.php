27c4d03bf9001c1f0896d5e231d8247903457011
Ivan Almonacid <ivan@cosof.cl>
2016-03-16 10:51:09 -0300

    Correccion en columnas para excel de casos febriles

--- application/controllers/formulario.php
+++ application/controllers/formulario.php
@@ -187,8 +187,9 @@ class Formulario extends MY_Controller
             foreach($lista as $caso){
                 $datos_excel[] = Zend_Json::decode($caso["propiedades"]);
                 $datos_excel[count($datos_excel)-1]["id"] = $caso["id"];
+                /*
                 $datos_excel[count($datos_excel)-1]["id_usuario"] = $caso["id_usuario"];
-                $datos_excel[count($datos_excel)-1]["id_estado"]  = $caso["id_estado"];
+                $datos_excel[count($datos_excel)-1]["id_estado"]  = $caso["id_estado"];*/
             }
         
             $excel = $this->excel->nuevoExcel();
@@ -207,7 +208,7 @@ class Formulario extends MY_Controller
             $excel->setActiveSheetIndex(0)->setCellValueByColumnAndRow(1, 1, "ESTADO"); 
             $excel->setActiveSheetIndex(0)->setCellValueByColumnAndRow(2, 1, "MÉDICO"); 
             
-            $i = 2;
+            $i = 3;
             foreach($columnas as $columna => $valor){
                 
                 $exportar = $this->_boExportarColumnaExcel($columna);
@@ -226,7 +227,7 @@ class Formulario extends MY_Controller
                 $excel->setActiveSheetIndex(0)->setCellValueByColumnAndRow(1, $j, (string) nombreFormularioEstado($valores["id_estado"]));
                 $excel->setActiveSheetIndex(0)->setCellValueByColumnAndRow(2, $j, (string) nombreUsuario($valores["id_usuario"]));
                 
-                unset($valores["id_usuario"]);
+                //unset($valores["id_usuario"]);
                 
                 $i = 3;
                 foreach($valores as $columna => $valor){
@@ -240,7 +241,7 @@ class Formulario extends MY_Controller
             }
 
             header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-            header('Content-Disposition: attachment;filename="casos_febriles.xlsx"');
+            header('Content-Disposition: attachment;filename="casos_febriles_'.date('d-m-Y').'.xlsx"');
             header('Cache-Control: max-age=0');
             $objWriter = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
             $objWriter->save('php://output');
