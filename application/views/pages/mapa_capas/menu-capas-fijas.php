71961bc7f8aec16be54c8649bfb48c103155c63b
Carlos Ayala <carlos.ayala@cosof.cl>
2016-05-03 16:39:28 -0400

    Icono

--- application/views/pages/mapa_capas/menu-capas-fijas.php
+++ application/views/pages/mapa_capas/menu-capas-fijas.php
@@ -23,7 +23,7 @@
         <li>
             <a id="btn-importar-marea-roja" href="javascript:void(0)">
                 <input type="checkbox" name="vectores" id="vectores" value="1"/> 
-                <i class="fa"><img width="20px" src="<?php echo base_url("assets/img/markers/marisco/rojo.png") ?>"></i> Vectores
+                <i class="fa"><img width="20px" src="<?php echo base_url("assets/img/markers/vectores.png") ?>"></i> Vectores
             </a>
         </li>
     </ul>
