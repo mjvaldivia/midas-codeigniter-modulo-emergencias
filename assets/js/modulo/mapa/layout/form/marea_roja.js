f1b8d6897d696fa79b50dd294713b4866b0ca7d1
Carlos Ayala <carlos.ayala@cosof.cl>
2016-05-16 12:56:32 -0400

    cambio

--- assets/js/modulo/mapa/layout/form/marea_roja.js
+++ assets/js/modulo/mapa/layout/form/marea_roja.js
@@ -50,7 +50,7 @@ var MapaLayoutFormMareaRoja = Class({
                 $("#marea_roja_resultados").ionRangeSlider({
                     type: "double",
                     min: 0,
-                    max: 12000,
+                    max: 30000,
                     grid: true,
                     step: 10,
                     keyboard: true,
