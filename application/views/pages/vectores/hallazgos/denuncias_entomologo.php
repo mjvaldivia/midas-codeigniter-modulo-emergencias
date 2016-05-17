072cad3d37f80b805b5f86ebb8fb964c5f8d62d3
Ivan Almonacid <ivan@cosof.cl>
2016-05-17 11:09:01 -0400

    Correccion para inspecciones

--- application/views/pages/vectores/hallazgos/denuncias_entomologo.php
+++ application/views/pages/vectores/hallazgos/denuncias_entomologo.php
@@ -1,11 +1,11 @@
 <div class="row">
     <div class="col-lg-12">
         <div class="page-title">
-            <h1> Gestión de Vigilancia de Vectores :: Hallazgos</h1>
+            <h1> Gestión de Vigilancia de Vectores :: Inspecciones</h1>
             <ol class="breadcrumb">
                 <li><i class="fa fa-dashboard"></i><a href="#"> Inicio </a></li>
                 <li><i class="fa fa-bell"></i> Vectores</li>
-                <li class="active"><i class="fa fa-bell"></i> Hallazgos</li>
+                <li class="active"><i class="fa fa-bell"></i> Inspecciones</li>
             </ol>
         </div>
     </div>
@@ -20,7 +20,7 @@
                     <input type="hidden" name="id" id="id" value="<?php echo $id ?>"/>
                     <div class="row">
                         <div class="col-xs-12">
-                            <legend>Revisión del hallazgo
+                            <legend>Revisión de la inspección
 
                             </legend>
 
