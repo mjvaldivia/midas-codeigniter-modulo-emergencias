9a55316636adaef0d8d7d1eaa772b7f24102d986
Ivan Almonacid <ivan@cosof.cl>
2016-05-18 14:03:20 -0400

    Revision para presidencia en vectores denuncias

--- application/views/pages/vectores/denuncias/index.php
+++ application/views/pages/vectores/denuncias/index.php
@@ -12,18 +12,22 @@
 </div>
 
 <div class="row">
-    <?php if(1):?>
-    <div class="col-xs-12 text-right">
-        <a href="<?php echo base_url('vectores/excel')?>" class="btn btn-primary btn-square" target="_blank"><i class="fa fa-file-excel-o"></i> Descargar Excel</a>
-        <a href="<?php echo base_url('vectores/denuncias')?>" class="btn btn-success btn-square"><i class="fa fa-plus"></i> Nueva Denuncia</a>
-    </div>
-    <?php endif;?>
+    <?php if (1): ?>
+        <div class="col-xs-12 text-right">
+            <a href="<?php echo base_url('vectores/excel') ?>" class="btn btn-primary btn-square" target="_blank"><i
+                    class="fa fa-file-excel-o"></i> Descargar Excel</a>
+            <?php if (!$presidencia): ?>
+                <a href="<?php echo base_url('vectores/denuncias') ?>" class="btn btn-success btn-square"><i
+                        class="fa fa-plus"></i> Nueva Denuncia</a>
+            <?php endif; ?>
+        </div>
+    <?php endif; ?>
     <div class="col-xs-12" style="margin-top:15px">
         <div class="portlet portlet-basic">
             <div class="portlet-body">
 
                 <div class="table-responsive" id="contenedor-lista-denuncias">
-                    <?php echo $grilla;?>
+                    <?php echo $grilla; ?>
                 </div>
             </div>
         </div>
