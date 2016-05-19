943dfabe5eb479906fd47516db3bff7d1f2433a7
Carlos Ayala <carlos.ayala@cosof.cl>
2016-05-19 10:37:28 -0400

    Correccion

--- application/helpers/modulo/laboratorio/form_helper.php
+++ application/helpers/modulo/laboratorio/form_helper.php
@@ -28,14 +28,22 @@ function formSelectLaboratorioUsuario($nombre, $atributos, $seleccion){
     $_ci =& get_instance();
     $_ci->load->library("core/form/form_select");
     $_ci->load->model("laboratorio_model", "_laboratorio_model");
-    $_ci->form_select->setNombre($nombre);
-    $_ci->form_select->populate(
-        $_ci->_laboratorio_model->listar(
-            array("usuario" => $_ci->session->userdata('session_idUsuario'))
-        )
+    
+    $lista = $_ci->_laboratorio_model->listar(
+        array("usuario" => $_ci->session->userdata('session_idUsuario'))
     );
-    $_ci->form_select->setOptionId("id");
-    $_ci->form_select->setOptionName("nombre");
-    $_ci->form_select->setAtributos($atributos);
-    return $_ci->form_select->render($nombre, $seleccion);
+    
+    if(!is_null($lista)){
+    
+        $_ci->form_select->setNombre($nombre);
+        $_ci->form_select->populate(
+            $_ci->_laboratorio_model->listar($lista)
+        );
+        $_ci->form_select->setOptionId("id");
+        $_ci->form_select->setOptionName("nombre");
+        $_ci->form_select->setAtributos($atributos);
+        return $_ci->form_select->render($nombre, $seleccion);
+    } else {
+        echo "<div class=\"alert alert-warning\"> No existen laboratorios asociados a su cuenta </div>";
+    }
 }
